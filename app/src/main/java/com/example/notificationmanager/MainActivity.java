8622faa4a6100092997a6e0850b21ead7472c1b7
Ayca Dogan <aycadgan@hotmail.com>
2019-05-08 11:46:04 +0200

    Created Notification

--- app/src/main/java/com/example/notificationmanager/MainActivity.java
+++ app/src/main/java/com/example/notificationmanager/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.notificationmanager;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, "channelId")
+                .setSmallIcon(R.drawable.ic_notification).setContentTitle("SUNNY!").setContentText("HELLO SUMMER!");
+        //tek satıra indirgeme: builder.setSmallIcon(1).setContentTitle("").setContentText("");
+
+        final View root = findViewById(R.id.activity_main__cl__root);
+        root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "background clicked");
+                NotificationManager notifManager = (NotificationManager) getSystemService(Service.NOTIFICATION_SERVICE);
+                notifManager.notify(1, builder.build());
+            }
+        });
     }
 }
